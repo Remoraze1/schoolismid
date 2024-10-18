d141c1463a4b86166eaeec346831c0b3f214f6ec
nothingrem1 <160821424+Remoraze1@users.noreply.github.com>
2024-10-18 15:00:37 +1030

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Array of texts to display
 const texts = [
-  'BallsyG!',
-  'BallsyG for justice!',
-  'A dozen a dime',
-  'BallsyG, the newest concrete type!',
-  'GitHub is unblocked on FortiGuard, I think!',
-  'BallsyG is unblocked!'
+    'BallsyG!',
+    'BallsyG for justice!',
+    'A dozen a dime',
+    'BallsyG, the newest concrete type!',
+    'GitHub is unblocked on FortiGuard I think',
+    'BallsyG is unblocke'
 ];
 
 // Initial index for rotating text
@@ -13,11 +13,12 @@ let index = 0;
 
 // Function to change text every 10 seconds
 function changeText() {
-  document.getElementById('text-display').innerText = texts[index];
-  index = (index + 1) % texts.length; // Loop back to start when the end is reached
+    document.getElementById('text-display').innerText = texts[index];
+    index = (index + 1) % texts.length; // Loop back to start when the end is reached
 }
 
 // Call the function every 10 seconds
 changeText(); // Initial call to show the first text immediately
 setInterval(changeText, 10000); // Change every 10,000 milliseconds (10 seconds)
+
 
